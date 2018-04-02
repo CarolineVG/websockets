cef61a710d19506a9e22246542431a1f68d1b13a
Caroline VG <caroline.vangossum@live.be>
2018-04-02 08:50:39 +0200

    made createpoll page and added route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+/* variabelen die doorgegeven moeten worden */
+var question, answer1, answer2;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* GET createpoll page. */
+router.get('/createpoll', function(req, res, next) {
+  res.render('createpoll', { title: 'Create Poll' });
+});
+
+/* POST data to createpoll page */
+router.post('/createpoll',function(req,res,next){
+  question = req.body.question; 
+  answer1 = req.body.answer1;
+  answer2 = req.body.answer2;
+  res.redirect('/');
 });
 
 module.exports = router;
