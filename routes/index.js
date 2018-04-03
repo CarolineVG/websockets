9d420fc034b2fcb848feb2bc6d276308ee098381
Caroline VG <caroline.vangossum@live.be>
2018-04-03 09:52:46 +0200

    remove test pages

--- routes/index.js
+++ routes/index.js
@@ -7,16 +7,8 @@ var assert = require('assert');
 
 var url = 'mongodb://localhost:27017/kweeni'; // run mongo terminal, read 2nd line  + /databasename
 
-/* variabelen die doorgegeven moeten worden */
-var question, answer1, answer2;
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home', question: question, answer1: answer1, answer2: answer2});
-});
-
-/* GET get page + data */
-router.get('/get', function (req, res, next) {
+/* GET home page + data */
+router.get('/', function (req, res, next) {
   // global var for item
   var item; 
   // connect to database
@@ -34,7 +26,7 @@ router.get('/get', function (req, res, next) {
     }, function () {
       // callback -> after: close db, render get page with item
       db.close();
-      res.render('get', { title: 'Home', question: item.question, answer1: item.answer1, answer2: item.answer2});
+      res.render('index', { title: 'Home', question: item.question, answer1: item.answer1, answer2: item.answer2});
     });
   });
 });
@@ -45,44 +37,29 @@ router.get('/createpoll', function(req, res, next) {
 });
 
 /* POST data to createpoll page */
-router.post('/createpoll',function(req,res,next){
-  question = req.body.question; 
-  answer1 = req.body.answer1;
-  answer2 = req.body.answer2;
-  res.redirect('/');
-});
+  router.post('/createpoll',function(req,res,next){
+    // create item
+    var item = {
+      question: req.body.question,
+      answer1: req.body.answer1,
+      answer2: req.body.answer2
+    };
 
-
-/* GET insert page. */
-router.get('/insert', function(req, res, next) {
-  res.render('insert', { title: 'Insert'});
-});
-
-/* POST data into insert page */
-router.post('/insert', function (req, res, next) {
-  // create item
-  var item = {
-    question: req.body.question,
-    answer1: req.body.answer1,
-    answer2: req.body.answer2
-  };
-
-  // connect to mongo db
-  mongo.connect(url, function (err, db) {
-    assert.equal(null, err);
-    // access database, use collection to insert item 
-    db.db('kweeni').collection('questions').insertOne(item, function (err, result) {
-      // callback (if no errors)
+    // connect to mongo db
+    mongo.connect(url, function (err, db) {
       assert.equal(null, err);
-      console.log('--- Item inserted ---');
-      console.log(item); 
-      db.close();
+      // access database, use collection to insert item 
+      db.db('kweeni').collection('questions').insertOne(item, function (err, result) {
+        // callback (if no errors)
+        assert.equal(null, err);
+        console.log('--- Item inserted ---');
+        console.log(item); 
+        db.close();
+      });
     });
-  });
 
-  // redirect to home page
-  res.redirect('/');
+    // redirect to home page
+    res.redirect('/');
 });
-
 
 module.exports = router;
