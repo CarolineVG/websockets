a57b3b79b0efee70d99af978fb1a39688426b373
Caroline VG <caroline.vangossum@live.be>
2018-04-03 09:24:28 +0200

    create insert page + insert item into database

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 var mongo = require('mongodb');
 var assert = require('assert');
 
-var url = 'mongodb://localhost:27017/test'; // run mongo terminal, read 2nd line  + /databasename
+var url = 'mongodb://localhost:27017/kweeni'; // run mongo terminal, read 2nd line  + /databasename
 
 /* variabelen die doorgegeven moeten worden */
 var question, answer1, answer2;
@@ -27,5 +27,36 @@ router.post('/createpoll',function(req,res,next){
   answer2 = req.body.answer2;
   res.redirect('/');
 });
+
+
+/* GET insert page. */
+router.get('/insert', function(req, res, next) {
+  res.render('insert', { title: 'Insert'});
+});
+
+/* POST data into insert page */
+router.post('/insert', function (req, res, next) {
+  // create item
+  var item = {
+    question: req.body.question,
+    answer1: req.body.answer1,
+    answer2: req.body.answer2
+  };
+  // connect to mongo db
+  mongo.connect(url, function (err, db) {
+    assert.equal(null, err);
+    // access database, use collection to insert item 
+    db.db('kweeni').collection('questions').insertOne(item, function (err, result) {
+      // callback (if no errors)
+      assert.equal(null, err);
+      console.log('Item inserted');
+      console.log(item); 
+      db.close();
+    });
+  });
+  // redirect to home page
+  res.redirect('/');
+});
+
 
 module.exports = router;
